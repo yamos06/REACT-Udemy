e437d44161d586086dae1e83aa6045d5685dfb6c
yamos06 <analauracampoy@gmail.com>
2022-12-02 23:01:08 -0300

    Módulo 1 Concluido

---
+++ 1 - Introdução/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h3>Tarefa 1 da Introdução concluida</h3>
+    </div>
+  );
+}
+
+export default App;
